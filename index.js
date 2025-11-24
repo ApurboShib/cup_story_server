f7734646a1df041b74830d5a487c8987b2798cc2
ApurboShib <shibjoy088@gmail.com>
2025-11-24 14:58:15 +0600

    added the full curd operation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require("cors")
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -11,11 +12,9 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-// user name : cup_story_server_db
-// user password : nMmSq7c8PHDkMcDj
 
-//console.log(process.env.DB_USER)
-//console.log(process.env.DB_PASS)
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vs8p1m5.mongodb.net/?appName=Cluster0`;
 //const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.vs8p1m5.mongodb.net/?appName=Cluster0";
@@ -33,6 +32,64 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+
+    const coffiesCollection = client.db('coffeeDB').collection('coffees')
+
+    // here we implement the get data.
+
+    app.get('/coffees', async (req, res) => {
+      // const cursor = coffiesCollection.find()
+      // const result = await cursor.toArray()
+
+      const result = await coffiesCollection.find().toArray();
+      res.send(result);
+    })
+
+    // here we implement the post data.
+
+    app.post('/coffees', async (req, res) => {
+      const newCoffeeData = req.body;
+      console.log(newCoffeeData);
+      const result = await coffiesCollection.insertOne(newCoffeeData)
+      //console.log(result);
+      res.send(result)
+    })
+
+    // for single coffee itam.
+
+    app.get('/coffees/:_id', async (req, res) => {
+      const id = req.params._id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffiesCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    })
+
+    // here we implement the update data.
+
+    app.put('/coffees/:_id', async (req, res) => {
+      const id = req.params._id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffiesCollection.updateOne(query, { $set: req.body })
+      console.log(result);
+      res.send(result)
+    })
+
+
+
+    // here we write the delete script.
+
+    app.delete('/coffee/:_id', async (req, res) => {
+      const id = req.params._id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffiesCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
@@ -44,9 +101,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send(' cup_story server is created.')
+
+  res.send(' cup_story server is created.')
 })
 
 app.listen(port, () => {
-    console.log(`The server is running on ${port}`)
+  console.log(`The server is running on ${port}`)
 })
